55afe7bd0ba934a8205178e21458a183cd1f8389
Abhishek jana <jana.abhishek123@gmail.com>
2022-12-15 21:28:40 +0530

    1st bug fix
    removed unwanted city search and null or undefined searches

--- techGun.js
+++ techGun.js
@@ -6,30 +6,52 @@ const options = {
   },
 };
 const getWeather = (city) => {
-  cityName.innerHTML = city;
+  // cityName.innerHTML = city;
+  if (city === "") {
+    city = "Delhi";
+  }
   fetch(
     "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
     options
   )
     .then((response) => response.json())
     .then((response) => {
-      console.log(response);
+      console.log("This is my response" + response.value);
 
-      //cloud_pct.innerHTML = response.cloud_pct;
-      temp.innerHTML = response.temp;
-      temp2.innerHTML = response.temp;
-      feels_like.innerHTML = response.feels_like;
-      humidity.innerHTML = response.humidity;
-      humidity2.innerHTML = response.humidity;
-      min_temp.innerHTML = response.min_temp;
-      max_temp.innerHTML = response.max_temp;
-      wind_speed.innerHTML = response.wind_speed;
-      wind_speed2.innerHTML = response.wind_speed;
-      wind_degrees.innerHTML = response.wind_degrees;
-      sunrise.innerHTML = response.sunrise;
-      sunset.innerHTML = response.sunset;
+      if (response.temp !== undefined) {
+        //cloud_pct.innerHTML = response.cloud_pct;
+        cityName.innerHTML = city;
+        temp.innerHTML = response.temp;
+        temp2.innerHTML = response.temp;
+        feels_like.innerHTML = response.feels_like;
+        humidity.innerHTML = response.humidity;
+        humidity2.innerHTML = response.humidity;
+        min_temp.innerHTML = response.min_temp;
+        max_temp.innerHTML = response.max_temp;
+        wind_speed.innerHTML = response.wind_speed;
+        wind_speed2.innerHTML = response.wind_speed;
+        wind_degrees.innerHTML = response.wind_degrees;
+        sunrise.innerHTML = response.sunrise;
+        sunset.innerHTML = response.sunset;
+      } else {
+        cityName.innerHTML = "Please fill the correct name of the city";
+        temp.innerHTML = "";
+        temp2.innerHTML = "";
+        feels_like.innerHTML = "";
+        humidity.innerHTML = "";
+        humidity2.innerHTML = "";
+        min_temp.innerHTML = "";
+        max_temp.innerHTML = "";
+        wind_speed.innerHTML = "";
+        wind_speed2.innerHTML = "";
+        wind_degrees.innerHTML = "";
+        sunrise.innerHTML = "";
+        sunset.innerHTML = "";
+      }
     })
-    .catch((err) => console.error(err));
+    .catch((err) => {
+      console.error("This is the error" + err);
+    });
 };
 
 submit.addEventListener("click", (e) => {
