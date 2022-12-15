4b829f4a3728d248096f932409bee85596773b12
Abhishek jana <jana.abhishek123@gmail.com>
2022-12-15 19:04:40 +0530

    My Weather App

---
+++ techGun.js
@@ -0,0 +1,137 @@
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "23daf53b9dmshc3a38038f690d5ap11ff13jsn93e59a55af2c",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
+};
+const getWeather = (city) => {
+  cityName.innerHTML = city;
+  fetch(
+    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+
+      //cloud_pct.innerHTML = response.cloud_pct;
+      temp.innerHTML = response.temp;
+      temp2.innerHTML = response.temp;
+      feels_like.innerHTML = response.feels_like;
+      humidity.innerHTML = response.humidity;
+      humidity2.innerHTML = response.humidity;
+      min_temp.innerHTML = response.min_temp;
+      max_temp.innerHTML = response.max_temp;
+      wind_speed.innerHTML = response.wind_speed;
+      wind_speed2.innerHTML = response.wind_speed;
+      wind_degrees.innerHTML = response.wind_degrees;
+      sunrise.innerHTML = response.sunrise;
+      sunset.innerHTML = response.sunset;
+    })
+    .catch((err) => console.error(err));
+};
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  getWeather(city.value);
+});
+
+getWeather("Delhi");
+
+//Fetch for Shanghai
+
+fetch(
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Shanghai",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+    cloud_pctShanghai.innerHTML = response.cloud_pct;
+    tempShanghai.innerHTML = response.temp;
+
+    feels_likeShanghai.innerHTML = response.feels_like;
+    humidityShanghai.innerHTML = response.humidity;
+
+    min_tempShanghai.innerHTML = response.min_temp;
+    max_tempShanghai.innerHTML = response.max_temp;
+    wind_speedShanghai.innerHTML = response.wind_speed;
+
+    wind_degreesShanghai.innerHTML = response.wind_degrees;
+    sunriseShanghai.innerHTML = response.sunrise;
+    sunsetShanghai.innerHTML = response.sunset;
+  })
+  .catch((err) => console.error(err));
+
+//Fetch for Boston
+fetch(
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Boston",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+    cloud_pctBoston.innerHTML = response.cloud_pct;
+    tempBoston.innerHTML = response.temp;
+
+    feels_likeBoston.innerHTML = response.feels_like;
+    humidityBoston.innerHTML = response.humidity;
+
+    min_tempBoston.innerHTML = response.min_temp;
+    max_tempBoston.innerHTML = response.max_temp;
+    wind_speedBoston.innerHTML = response.wind_speed;
+
+    wind_degreesBoston.innerHTML = response.wind_degrees;
+    sunriseBoston.innerHTML = response.sunrise;
+    sunsetBoston.innerHTML = response.sunset;
+  })
+  .catch((err) => console.error(err));
+
+//Fetch for Lucknow
+fetch(
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Lucknow",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+    cloud_pctLucknow.innerHTML = response.cloud_pct;
+    tempLucknow.innerHTML = response.temp;
+
+    feels_likeLucknow.innerHTML = response.feels_like;
+    humidityLucknow.innerHTML = response.humidity;
+
+    min_tempLucknow.innerHTML = response.min_temp;
+    max_tempLucknow.innerHTML = response.max_temp;
+    wind_speedLucknow.innerHTML = response.wind_speed;
+
+    wind_degreesLucknow.innerHTML = response.wind_degrees;
+    sunriseLucknow.innerHTML = response.sunrise;
+    sunsetLucknow.innerHTML = response.sunset;
+  })
+  .catch((err) => console.error(err));
+
+//Fetch for Kolkata
+fetch(
+  "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Kolkata",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+    cloud_pctKolkata.innerHTML = response.cloud_pct;
+    tempKolkata.innerHTML = response.temp;
+
+    feels_likeKolkata.innerHTML = response.feels_like;
+    humidityKolkata.innerHTML = response.humidity;
+
+    min_tempKolkata.innerHTML = response.min_temp;
+    max_tempKolkata.innerHTML = response.max_temp;
+    wind_speedKolkata.innerHTML = response.wind_speed;
+
+    wind_degreesKolkata.innerHTML = response.wind_degrees;
+    sunriseKolkata.innerHTML = response.sunrise;
+    sunsetKolkata.innerHTML = response.sunset;
+  })
+  .catch((err) => console.error(err));
